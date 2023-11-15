c5d21fab31adbb3a5f453d2fedd6ce3da273ba72
Benjamin Knobloch <bknobloc@stevens.edu>
2023-11-15 17:45:23 -0500

    Create VeggiePizza.java

---
+++ AbstractFactory/src/VeggiePizza.java
@@ -0,0 +1,20 @@
+public class VeggiePizza extends Pizza {
+	PizzaIngredientFactory ingredientFactory;
+ 
+	public VeggiePizza(PizzaIngredientFactory ingredientFactory) {
+		this.ingredientFactory = ingredientFactory;
+	}
+ 
+	void prepare() {
+		System.out.println("Preparing " + name);
+		dough = ingredientFactory.createDough();
+		sauce = ingredientFactory.createSauce();
+		cheese = ingredientFactory.createCheese();
+		
+        veggies = new Veggies[4];
+        veggies[0] = new Garlic();
+        veggies[1] = new Mushroom();
+        veggies[2] = new Onion();
+        veggies[3] = new RedPepper();
+	}
+}

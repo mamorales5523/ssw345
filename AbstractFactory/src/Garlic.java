4c7d6d6f00420cbccda7644c8234520b70f15d3b
mamorales5523 <90879620+mamorales5523@users.noreply.github.com>
2023-11-28 11:53:14 -0500

    decorator pattern and singleton hw

--- AbstractFactory/src/Garlic.java
+++ AbstractFactory/src/Garlic.java
@@ -2,4 +2,4 @@ public class Garlic implements Veggies {
     public String toString() {
 		return "Garlic veggie topping";
 	}
-}
+} 
---
+++ DecoratorPattern/src/MargheritaPizza.java
@@ -0,0 +1,9 @@
+public class MargheritaPizza implements Pizza {
+    public String getDescription() {
+		return "MargheritaPizza";
+	}
+    
+    public double getCost() {
+    	return 6.99;
+    }
+} 
